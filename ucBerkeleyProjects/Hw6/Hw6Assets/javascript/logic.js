be3e50044399c2792b7b609c2c61f88a4dae6ff4
Christian Maldonado <chris.maldonado99@gmail.com>
2020-01-13 00:32:05 -0800

    fixed some issues with homework 6

--- ucBerkeleyProjects/Hw6/Hw6Assets/javascript/logic.js
+++ ucBerkeleyProjects/Hw6/Hw6Assets/javascript/logic.js
@@ -1,37 +1,38 @@
-// hero array
-var hero = ["Captain America", "Hulk", "Iron Man", "Thor", "Black Widow", "Hawkeye"]
 
-function gifDisplay() {
+// hero array
+var hero = ["Captain America", "Hulk", "Iron Man", "Thor"];
+
+function displayGif() {
 
     var hero = $(this).attr("data-name");
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=PzWVIo3ocW3i5iJKFy7ceDRxGL2hMfxd";
+    var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=PzWVIo3ocW3i5iJKFy7ceDRxGL2hMfxd=10";
 
     //Ajax Call
     $.ajax({ url: queryUrl, method: "GET"}).done(function(response) {
 
-        //Empty div holdign previous Gifs
-        $('gifStorage').empty();
+        //Empty div holding previous Gifs
+        $('.gifGoesHere').empty();
 
-        for (let i = 0; i < response.data.length; i++){
+        for (i = 0; i < response.data.length; i++){
 
             // Variables to add images by using the button created in index.HTML
             var gifDiv    = $('<div class="gifDiv">');
             var rating    = response.data[i].rating;
             var ratingDiv = $('<p>').html("Rating: " + rating);
             var animated  = response.data[i].images.fixed_height.url;
-            var still     = respomse.data[i].images.fixes_height_still.url;
-            var gifImg    = $('<img calss="gImage">');
+            var still     = respomse.data[i].images.fixed_height_still.url;
+            var gifImg    = $('<img class="gImage">');
 
             // Default gifs still
             gifImg.attr('src', still);
-            gifImg.attr('data'-still, still);
+            gifImg.attr('data-still', still);
             gifImg.attr('data-animate', animated);
             gifImg.attr('data-state', 'still')
 
             // Securing ratings appear after their respective gifs
             gifDiv.append(ratingDiv);
             gifDiv.prepend(gifImg);
-            $('.gifStorage').prepend(gifDiv);
+            $('.gifgoesHere').prepend(gifDiv);
 
         } 
 
@@ -41,7 +42,7 @@ function gifDisplay() {
 
 // on Click funciton to pause/resume GIF
 
-$('.gifStorage').on("click", ".gImage", function() {
+$('.gifGoesHere').on("click", ".gImage", function() {
 
     var state = $(this).attr('data-state');
     // If state = still, on click will resume the GIF
@@ -51,6 +52,7 @@ $('.gifStorage').on("click", ".gImage", function() {
         $(this).attr('src', $(this).data('animate'));
         $(this).attr('data-state', 'animate');
     }
+    
     else {
 
         $(this).attr('src', $(this).data('still'));
@@ -65,15 +67,15 @@ function renderButtons() {
     $(".buttons-original").empty();
 
     // Verifying buttons
-    for ( i = 0; i < hero.length; i++)
-        addButton = $('<button class = "button">');
+    for ( i = 0; i < hero.length; i++) {
+        addButton = $('<button class="button">');
         addButton.addClass("hero");
         addButton.attr("data-name", hero[i]);
         addButton.html(hero[i]);
 
         // Append newly created buttons to the end of existing buttons
         $(".buttons-original").append(addButton);
-
+    }
 };
 
 // New button created when field has a value and then clearing it 
@@ -81,13 +83,13 @@ function renderButtons() {
 $(".add-gif").on("click", function(event) {
 
     event.preventDefault();
-    var gifs = $(".gifName").val().trim();
+    var gifs = $(".gif-name").val().trim();
     hero.push(gifs);
-    $(".gifName").val("");
+    $(".gif-name").val("");
     renderButtons();
 
 });
 
 // Call button in class of hero
-$(document).on("click", ".hero", gifDisplay);
+$(document).on("click", ".hero", displayGif);
 renderButtons();
