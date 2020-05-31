13f4b092029717fbef3c126235f7e87dd330cbbb
Christian Maldonado <chris.maldonado99@gmail.com>
2020-05-30 19:54:34 -0700

    Updates

---
+++ precache-manifest.538638ff9041e910306a39edb05f9875.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b49999040df506a11d2f074c9e723c",
+    "url": "/christianlMaldonado/christianMaldonadoPortfolio/index.html"
+  },
+  {
+    "revision": "777064bc5bd9222f6325",
+    "url": "/christianlMaldonado/christianMaldonadoPortfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b4a7a399f7be100d5c62",
+    "url": "/christianlMaldonado/christianMaldonadoPortfolio/static/css/main.d79695cf.chunk.css"
+  },
+  {
+    "revision": "777064bc5bd9222f6325",
+    "url": "/christianlMaldonado/christianMaldonadoPortfolio/static/js/2.eb6c2a25.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/christianlMaldonado/christianMaldonadoPortfolio/static/js/2.eb6c2a25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a7a399f7be100d5c62",
+    "url": "/christianlMaldonado/christianMaldonadoPortfolio/static/js/main.d685792d.chunk.js"
+  },
+  {
+    "revision": "5de71fc4ed35ffa3d47f",
+    "url": "/christianlMaldonado/christianMaldonadoPortfolio/static/js/runtime-main.6acf8dcd.js"
+  },
+  {
+    "revision": "9ccf6a27dce216b1f774214e7ebf16d1",
+    "url": "/christianlMaldonado/christianMaldonadoPortfolio/static/media/image1.9ccf6a27.jpg"
+  },
+  {
+    "revision": "62d7d7dded871121278246cb4f479471",
+    "url": "/christianlMaldonado/christianMaldonadoPortfolio/static/media/logo.62d7d7dd.jpg"
+  }
+]);
